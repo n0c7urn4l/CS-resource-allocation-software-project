cbb18a3c02496b39b8a2c82f60fde2ed08096bbb
shanuka chathuranga <shanukachathuranga21@gmail.com>
2023-05-20 03:57:42 +0530

    finalize login gui

--- ResourceAllocationSoftware/src/resourceallocationsoftware/ResourceAllocationSoftware.java
+++ ResourceAllocationSoftware/src/resourceallocationsoftware/ResourceAllocationSoftware.java
@@ -4,6 +4,8 @@
  */
 package resourceallocationsoftware;
 
+
+
 /**
  *
  * @author shanu
@@ -14,7 +16,7 @@ public class ResourceAllocationSoftware {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         
     }
     
